2fd1d43138c7716ebef1277d889d34553ff8e9af
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2025-03-01 20:27:36 +0300

    chore: Remove unused OverflowContext

--- src/context/GlobalProvider.jsx
+++ src/context/GlobalProvider.jsx
@@ -1,10 +1,5 @@
-import OverflowProvider from "./OverflowContext"
 import { SectionInViewProvider } from "./SectionInViewContext"
 
 export default function GlobalProvider({ children }) {
-  return (
-    <SectionInViewProvider>
-      <OverflowProvider>{children}</OverflowProvider>
-    </SectionInViewProvider>
-  )
+  return <SectionInViewProvider>{children}</SectionInViewProvider>
 }
